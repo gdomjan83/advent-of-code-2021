96f4081b06a7220fb5e9908c9e81e22bd1a352e7
J_C <gomjan83@gmail.com>
2021-12-01 17:51:25 +0100

    Day1 puzzle1 finished.

---
+++ src/main/java/day01/ScanningDeep.java
@@ -0,0 +1,35 @@
+package day01;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class ScanningDeep {
+
+    public int countDeep(Path path) {
+        int count = 0;
+        List<String> lines = readFile(path);
+        for (int i = 0; i < lines.size() - 1; i++) {
+            if (Integer.parseInt(lines.get(i)) < Integer.parseInt(lines.get(i + 1))) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private List<String> readFile(Path path) {
+        try {
+            return Files.readAllLines(path);
+        }
+        catch (IOException ioe) {
+            throw new IllegalArgumentException("Cannot read file.");
+        }
+    }
+
+    public static void main(String[] args) {
+        ScanningDeep sd = new ScanningDeep();
+        System.out.println(sd.countDeep(Paths.get("src/main/resources/data.txt")));
+    }
+}
